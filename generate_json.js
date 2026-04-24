09e21b49fc40d70b41c5ada2fee6dc46de50f2df
Mahmoud Esawi <esawi@ieee.org>
2026-04-24 17:43:32 +0300

    chore: Clean up testing scripts, temporary files, and logs

--- generate_json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('seed.js', 'utf8');
-const startIndex = content.indexOf('const products = [');
-const endIndex = content.indexOf('];', startIndex) + 1;
-const productsStr = content.substring(startIndex + 'const products = '.length, endIndex);
-const products = eval(productsStr);
-fs.mkdirSync('data', { recursive: true });
-fs.writeFileSync('data/jordan_products.json', JSON.stringify(products, null, 2));
-console.log('Successfully generated data/jordan_products.json');
